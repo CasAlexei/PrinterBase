265a1397d345e168c78bf3b04fe632d2a2d45d6b
CasAlexei <hp81@mail.ru>
2020-11-15 17:57:58 +0200

    Add new line with println

--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -1,5 +1,9 @@
 public class MainForm {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
+
+        // создаем новые изменения в файле
+        System.out.println("New line in project");
     }
+    
 }
